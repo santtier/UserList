641e8893373a4116df2698bb099357664ccd5a90
santtier <santitodarello14@gmail.com>
2022-01-23 22:47:56 -0300

    Uploading Demo App

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+import AddUser from './components/Users/AddUser'
+import UsersList from './components/Users/UsersList'
+
+function App () {
+  const [userList, setUserList] = useState([])
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList(prevUsersList => {
+      return [
+        ...prevUsersList,
+        {
+          name: uName,
+          age: uAge,
+          id: Math.random().toString()
+        }
+      ]
+    })
+  }
+
+  return (
+    <>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={userList} />
+    </>
+  )
+}
+
+export default App
